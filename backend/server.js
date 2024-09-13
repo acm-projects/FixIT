5708fea066d7aed13f10cfc9fa1683bb9b7d5d2d
ua <anyugonna@gmail.com>
2024-09-13 12:28:39 -0500

    add database connection code

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
 // middleware - middle man between getting a request (e.g. GET request) and sending data
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
 // route handling
 app.get('/', (req, res) => {
-    res.json({mssg: "What's up"})
-})
+    res.json({mssg: "What's up"});
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}.`)
-})
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to database and server running on port ${process.env.PORT}.`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
